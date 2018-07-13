25165d88e2b3ff8134fbf8e9a14abea05e9e13a9
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 18:46:58 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # person.rb
+class Person  
+  attr_accessor :adele_goldberg, :alan_kay
+  def initialize(adele_goldberg, alan_kay)
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end
+end
+class_one = Person.new("adele_goldberg", "alan_kay")
+
+ puts class_one.adele_goldberg
+ puts class_one.alan_kay
