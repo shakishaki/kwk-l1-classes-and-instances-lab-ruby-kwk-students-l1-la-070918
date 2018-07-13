2e6d4e38a93e9bb5db81d6a0eea4928367a314cc
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 18:42:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dog 
+  attr_accessor :fido, :snoopy, :lassie
+  def initialize(fido, snoopy, lassie)
+    @fido = fido
+    @snoopy = snoopy
+    @lassie = lassie
+end
+
+class_one = Dog.new("fido","snoopy", "lassie")
+
+ puts class_one.fido
+ puts class_one.snoopy
+ puts class_one.lassie
+ 
+end
